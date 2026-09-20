4a8e78c0d872ebff660e9eacccbe1045018f9ceb
hmarufhossain420-max <hmarufhossain420@gmail.com>
2026-09-20 16:28:37 +0600

    add weather app file

---
+++ app.py
@@ -0,0 +1,29 @@
+import requests
+import streamlit as st
+
+st.title("🌤️ Simple Weather App MADE BY BC MARUF")
+st.write("a bukachuda city dis naile kick khabi")
+
+api = "d2634743380898849931b9a0fc9c3c1b"
+user_input = st.text_input("Enter your city:")
+
+if st.button("Get Weather"):
+    
+    if user_input:
+        
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&appid={api}"
+        weather_data = requests.get(url)
+        
+        
+        if weather_data.status_code == 200:
+            data = weather_data.json()
+            weather = data['weather'][0]['main']
+            temperature = data['main']['temp']
+            
+            
+            st.success(f"Weather in {user_input.title()}: **{weather}**")
+            st.metric(label="Temperature", value=f"{temperature} °F")
+        else:
+            st.error("City not found! Please check the spelling and try again.")
+    else:
+            st.warning("Please enter a city name first.")
